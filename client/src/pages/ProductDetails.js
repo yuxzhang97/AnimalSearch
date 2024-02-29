c9e03a713271c30b09b4e31305f86450b82ae833
Shawn <yxzhang2@illinois.edu>
2024-02-29 17:14:00 -0500

    feat: Implement fetching product details in ProductDetails component

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -1,18 +1,44 @@
-// ProductDetails.js
-
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useQuery, gql } from '@apollo/client';
+import { Box, Image, Heading, Text } from '@chakra-ui/react';
+
+const GET_PRODUCT_DETAILS = gql`
+  query GetProductDetails($_id: ID!) {
+    getProductByID(_id: $_id) {
+      _id
+      name
+      description
+      price
+      category
+      imageURL
+    }
+  }
+`;
 
 const ProductDetails = () => {
   const { productId } = useParams();
 
+  const { loading, error, data } = useQuery(GET_PRODUCT_DETAILS, {
+    variables: { _id: productId },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const { getProductByID: product } = data;
 
   return (
-    <div>
-      <h1>Product Details</h1>
-      <p>Product ID: {productId}</p>
-      {/* Render product details here */}
-    </div>
+    <Box maxW="800px" mx="auto" p="4">
+      <Heading as="h1" size="xl" mb="4">Product Details</Heading>
+      <Box borderWidth="1px" borderRadius="lg" overflow="hidden" p="4">
+        <Image src={product.imageURL} alt={product.name} mb="4" />
+        <Heading as="h2" size="lg" mb="2">{product.name}</Heading>
+        <Text fontSize="md" mb="2">{product.description}</Text>
+        <Text fontSize="lg" fontWeight="bold" mb="2">Price: ${product.price}</Text>
+        <Text fontSize="lg" fontStyle="italic">Category: {product.category}</Text>
+      </Box>
+    </Box>
   );
 };
 
