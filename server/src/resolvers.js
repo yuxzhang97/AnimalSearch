f9b49be1c397aa29b62bd15f86f5935809b51159
Shawn <yxzhang2@illinois.edu>
2024-02-29 16:14:07 -0500

    feat(backend): add support for searching products in the database
    - Updated GraphQL schema to include a new query field for searching products.
    - Implemented resolver function to filter products based on search query.
    - Added backend logic to perform search operations on the database.

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -2,7 +2,7 @@ const Product = require('./models/Product');
 
 const resolvers = {
   Query: {
-    products: async () => {
+    getAllProducts: async () => {
       try {
         const products = await Product.find();
         return products;
@@ -10,7 +10,7 @@ const resolvers = {
         throw new Error('Error fetching all products');
       }
     },
-    product: async (_, { _id }) => {
+    getProductByID: async (_, { _id }) => {
       try {
         const product = await Product.findById(_id);
         if (!product) {
@@ -21,14 +21,19 @@ const resolvers = {
         throw new Error('Error fetching product');
       }
     },
-    productsByCategory: async (_, { category }) => {
-      try {
-        const products = await Product.find({ category });
-        return products;
-      } catch (error) {
-        throw new Error('Error fetching products by category');
-      }
-    }
+    searchProducts: async (_, { query }) => {
+      // Filter products based on the search query
+      const regex = new RegExp(query, 'i'); // Case-insensitive search
+      const products = await Product.find({
+        $or: [
+          { name: { $regex: regex } },
+          { description: { $regex: regex } },
+          { category: { $regex: regex } },
+        ],
+      });
+      return products;
+    },
+    
   }
 };
 
--- server/src/schema.js
+++ server/src/schema.js
@@ -1,19 +1,29 @@
 const { gql } = require("apollo-server");
 
 const typeDefs = gql`
+  "A Product is an item in the store with different properties"
   type Product {
     _id: ID!
+    "The product's name"
     name: String!
+    "Some information abotu the product"
     description: String!
+    "The price of the product"
     price: Float!
+    "The category the product belongs to"
     category: String!
+    "The product's image to display on each product card or detail"
     imageURL: String!
   }
 
+  # Define the Query type with its fields
   type Query {
-    products: [Product]
-    product(_id: ID!): Product
-    productsByCategory(category: String!): [Product] 
+    "Query to retrieve all products"
+    getAllProducts: [Product]
+    "Query to retrieve a product by its ID"
+    getProductByID(_id: ID!): Product
+    "Query to search for products based on a search query"
+    searchProducts(query: String!): [Product]
   }
 `;
 
