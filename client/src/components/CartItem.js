760a8a37310357c17e8a192a9d64cf8e822a3430
Shawn <yxzhang2@illinois.edu>
2024-03-01 10:50:41 -0500

    minor adjustments to cart item dropdown

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -1,32 +1,40 @@
-import React from 'react';
-import { Flex, Image, Select, Button } from '@chakra-ui/react';
-import { useCart } from '../contexts/CartContext'; // Import useCart hook
-import { DeleteIcon } from '@chakra-ui/icons';
+import React from "react";
+import { Flex, Image, Select, Button } from "@chakra-ui/react";
+import { useCart } from "../contexts/CartContext";
+import { DeleteIcon } from "@chakra-ui/icons";
 
-const CartItem = ({ item, removeFromCart }) => {
-  const { updateQuantity } = useCart(); // Get updateQuantity function from useCart hook
+const CartItem = ({ item }) => {
+  const { updateQuantity, removeFromCart } = useCart();
 
   const handleQuantityChange = (event) => {
-    const newQuantity = event.target.value;
-    if (newQuantity === "0") {
+    const newQuantity = parseInt(event.target.value);
+    if (newQuantity === 0) {
       removeFromCart(item._id);
     } else {
-      updateQuantity(item._id, parseInt(newQuantity));
+      updateQuantity(item._id, newQuantity);
     }
   };
 
   return (
-    <Flex key={item._id} alignItems="center" mb="2" width="100%" justifyContent="center">
+    <Flex key={item._id} alignItems="center" mb="2" width="100%">
       <Flex flexDirection="column" justifyContent="center" alignItems="center">
         <Image src={item.imageURL} alt={item.name} boxSize="80px" mb="2" />
         <Flex justifyContent="space-between" alignItems="center" width="100%">
-          <Select size="sm" value={item.quantity} onChange={handleQuantityChange}>
-            <option value="0">0 (Delete)</option>
-            {[...Array(9).keys()].map((number) => (
-              <option key={number} value={number + 1}>{number + 1}</option>
+          <Select
+            size="sm"
+            value={item.quantity} // Always show the current quantity
+            onChange={handleQuantityChange}
+          >
+            <option value={0}>0 (Delete)</option>
+            {[...Array(10).keys()].map((number) => (
+              <option key={number} value={number}>
+                {number}
+              </option>
             ))}
           </Select>
-          <Button size="sm" onClick={() => removeFromCart(item._id)}><DeleteIcon /></Button>
+          <Button size="sm" onClick={() => removeFromCart(item._id)}>
+            <DeleteIcon />
+          </Button>
         </Flex>
       </Flex>
     </Flex>
