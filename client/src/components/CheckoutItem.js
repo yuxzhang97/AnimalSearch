26520f486e358754206f1231be3fca329ca9f656
Shawn <yxzhang2@illinois.edu>
2024-03-05 16:16:56 -0500

    feat(frontend): Added cart items to checkout page

---
+++ client/src/components/CheckoutItem.js
@@ -0,0 +1,156 @@
+import React, { useEffect, useState } from "react";
+import {
+  Flex,
+  Image,
+  Button,
+  Select,
+  Spinner,
+  Input,
+  Box,
+  Text
+} from "@chakra-ui/react";
+import { DeleteIcon } from "@chakra-ui/icons";
+import { useRemoveFromCart, useUpdateCartItem } from "../services/cartServices";
+import { useUser } from "../contexts/UserContext";
+import { useGetUserCart } from "../services/cartServices";
+
+// Cart Item Component
+const CheckoutItem = ({ item }) => {
+  const removeFromCart = useRemoveFromCart();
+  const updateCartItem = useUpdateCartItem();
+  const { userId } = useUser();
+  const { refetch } = useGetUserCart(userId); // Fetch refetch function from useGetUserCart
+  const [loading, setLoading] = useState(false);
+  const [quantity, setQuantity] = useState(item.quantity);
+  const [editMode, setEditMode] = useState(false);
+
+  const handleQuantityChange = async (newQuantity) => {
+    setLoading(true);
+    try {
+      await updateCartItem(
+        userId,
+        item.product._id,
+        parseInt(newQuantity),
+        refetch
+      );
+      setQuantity(newQuantity);
+    } catch (error) {
+      console.error("Error updating cart item:", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleDropdown = async (event) => {
+    if (event.target.value === "10+") {
+      setEditMode(true);
+    } else {
+      const newQuantity = parseInt(event.target.value);
+      handleQuantityChange(newQuantity);
+    }
+  };
+
+  const handleChangeInput = (event) => {
+    if (!editMode) {
+      setEditMode(true);
+    }
+    setQuantity(event.target.value);
+  };
+  const handleUpdateQuantity = async () => {
+    await handleQuantityChange(quantity);
+    setEditMode(false);
+  };
+
+  const handleRemoveFromCart = async () => {
+    setLoading(true);
+    try {
+      await removeFromCart(userId, item.product._id, refetch);
+      console.log("Product removed from cart successfully");
+    } catch (error) {
+      console.error("Error removing from cart:", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (!editMode) {
+      setQuantity(item.quantity);
+    }
+  });
+
+  return (
+    <Box
+      p="4"
+      mb="4"
+      width="70%"
+      border="1px solid #E2E8F0"
+      borderRadius="md"
+      boxShadow="md"
+      backgroundColor="white"
+    >
+      <Flex alignItems="center">
+        <Image
+          src={item.product.imageURL}
+          alt={item.product.name}
+          boxSize="100px"
+          mr="4"
+        />
+        <Flex flexDirection="column" flex="1">
+          <Text fontWeight="bold" mb="1">
+            {item.product.name}
+          </Text>
+          <Text fontSize="sm" mb="1">
+            {item.product.description}
+          </Text>
+          <Text fontSize="lg" fontWeight="bold" mb="1">
+            ${item.product.price}
+          </Text>
+          <Flex alignItems="center">
+            {loading ? (
+              <Spinner size="sm" />
+            ) : (
+              <>
+                {quantity > 9 || editMode ? (
+                  <Input
+                    size="sm"
+                    value={quantity}
+                    onChange={handleChangeInput}
+                    disabled={loading}
+                    width="8rem"
+                    textAlign="center"
+                  />
+                ) : (
+                  <Select
+                    size="sm"
+                    value={quantity}
+                    onChange={handleDropdown}
+                    disabled={loading}
+                    width={"8rem"}
+                    mr="2"
+                  >
+                    {[...Array(9).keys()].map((value) => (
+                      <option key={value} value={value + 1}>
+                        {value + 1}
+                      </option>
+                    ))}
+                    <option value="10+">10+</option>
+                  </Select>
+                )}
+                <Button
+                  size="sm"
+                  onClick={editMode ? handleUpdateQuantity : handleRemoveFromCart}
+                  disabled={loading}
+                >
+                  {editMode ? "Update" : <DeleteIcon />}
+                </Button>
+              </>
+            )}
+          </Flex>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+};
+
+export default CheckoutItem;
--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { useUser } from '../contexts/UserContext';
 import { useGetUserCart } from '../services/cartServices';
 import { useAddOrder } from '../services/orderServices';
-import { Button, Center, Spinner } from '@chakra-ui/react';
+import { Button, Center, Spinner, Box, Text } from '@chakra-ui/react';
+import CheckoutItem from '../components/CheckoutItem';
 
 const Checkout = () => {
   const { userId } = useUser(); // Get the current user from the context
-  const { loading, error, cartItems } = useGetUserCart(userId); // Use useGetUserCart hook to fetch cart items
+  const { loading, error, cartItems, refetch } = useGetUserCart(userId); // Use useGetUserCart hook to fetch cart items
   const { addOrder } = useAddOrder(); // Hook for adding an order
 
   const handleCheckout = async () => {
@@ -33,12 +34,17 @@ const Checkout = () => {
   };
 
   if (loading) return <Center><Spinner size="xl" /></Center>;
-  if (error) return <p>Error fetching cart data: {Error.message}</p>;
+  if (error) return <p>Error fetching cart data: {error.message}</p>;
 
   return (
     <Center flexDirection="column">
-      <h2>Checkout</h2>
-      <Button colorScheme="blue" onClick={handleCheckout}>Place Order</Button>
+      <Box width="100%">
+        <Text fontSize="xl" mb="4">Your Cart Items:</Text>
+        {cartItems.map((cartItem, index) => (
+          <CheckoutItem key={index} item={cartItem} refetch={refetch} />
+        ))}
+      </Box>
+      <Button colorScheme="blue" mt="4" onClick={handleCheckout}>Place Order</Button>
     </Center>
   );
 };
