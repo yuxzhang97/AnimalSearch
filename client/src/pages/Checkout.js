da5d304ac0f3f216d999108dcdaf80174e84ff66
Shawn <yxzhang2@illinois.edu>
2024-03-05 16:21:41 -0500

    feat(frontend): Added subtotal counter on checkout page

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { useUser } from '../contexts/UserContext';
-import { useGetUserCart } from '../services/cartServices';
-import { useAddOrder } from '../services/orderServices';
-import { Button, Center, Spinner, Box, Text } from '@chakra-ui/react';
-import CheckoutItem from '../components/CheckoutItem';
+import React from "react";
+import { useUser } from "../contexts/UserContext";
+import { useGetUserCart } from "../services/cartServices";
+import { useAddOrder } from "../services/orderServices";
+import { Button, Center, Spinner, Box, Text } from "@chakra-ui/react";
+import CheckoutItem from "../components/CheckoutItem";
 
 const Checkout = () => {
   const { userId } = useUser(); // Get the current user from the context
@@ -11,40 +11,59 @@ const Checkout = () => {
   const { addOrder } = useAddOrder(); // Hook for adding an order
 
   const handleCheckout = async () => {
-    console.log(userId);
     if (!cartItems || loading || error) return;
 
     try {
       // Extract items from cart data
-      const items = cartItems.map(cartItem => ({
+      const items = cartItems.map((cartItem) => ({
         quantity: cartItem.quantity,
         productId: cartItem.product._id,
       }));
 
-      console.log(items);
-
       // Add order
       await addOrder(userId, items);
 
-      console.log('Order placed successfully!');
+      console.log("Order placed successfully!");
     } catch (error) {
-      console.error('Error placing order:', error);
+      console.error("Error placing order:", error);
       // Display error message to the user
     }
   };
 
-  if (loading) return <Center><Spinner size="xl" /></Center>;
+  // Calculate subtotal by considering quantities
+  const subtotal = cartItems.reduce((acc, cartItem) => {
+    return acc + cartItem.quantity * cartItem.product.price;
+  }, 0);
+
+  // Calculate total number of items in the cart
+  const totalItems = cartItems.reduce((acc, cartItem) => {
+    return acc + cartItem.quantity;
+  }, 0);
+
+  if (loading)
+    return (
+      <Center>
+        <Spinner size="xl" />
+      </Center>
+    );
   if (error) return <p>Error fetching cart data: {error.message}</p>;
 
   return (
     <Center flexDirection="column">
       <Box width="100%">
-        <Text fontSize="xl" mb="4">Your Cart Items:</Text>
+        <Text fontSize="xl" mb="4">
+          Your Cart Items:
+        </Text>
         {cartItems.map((cartItem, index) => (
           <CheckoutItem key={index} item={cartItem} refetch={refetch} />
         ))}
       </Box>
-      <Button colorScheme="blue" mt="4" onClick={handleCheckout}>Place Order</Button>
+      <Text fontSize="xl" mt="4">
+        Subtotal ({totalItems} Items): ${subtotal.toFixed(2)}
+      </Text>
+      <Button colorScheme="blue" mt="4" onClick={handleCheckout}>
+        Place Order
+      </Button>
     </Center>
   );
 };
